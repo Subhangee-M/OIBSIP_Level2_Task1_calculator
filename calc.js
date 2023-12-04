b1db5aae5c80ed061208ef539c12aabce3379436
Subhangee Mishra <96335372+Subhangee-M@users.noreply.github.com>
2023-12-05 00:31:48 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,18 @@
+const clr = () => document.getElementById('result').value = " ";
+
+const display = (val) => {
+    document.getElementById('result').value += val;
+
+}
+
+const del = () => document.getElementById('result').value = (document.getElementById('result').value).slice(0, -1);
+let calculate = () => {
+    try {
+        let x = document.getElementById('result').value;
+        let y = eval(x);
+        document.getElementById('result').value = y;
+    } catch (error) {
+        alert('Please enter a valid expression.');
+        console.log(error);
+    }
+}
